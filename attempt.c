0b82ee61d5a7981ecf1d6478435b8ae737b2f7d4
Whrecker <142145359+Whrecker@users.noreply.github.com>
2023-08-15 10:20:23 +0530

    triangle

---
+++ attempt.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c,d=0;
+	a=b=c=d=0;
+	int z=5;
+	while(c<5)
+	{int e=5;
+		while(b<e)
+		{
+			printf(" ");
+			e--;
+		}
+		printf("o");
+		if(c>0)
+		{
+		int f=4;
+		while(z<f)
+			{
+				printf(" ");
+				f--;
+			}
+		}
+		if(b!=0){
+			printf("o");
+			}
+			printf("\n");
+	
+	b++;
+	c++;
+	d++;
+	z=z-2;
+	}
+	if(c>=5)
+		{
+			while(a<11)
+			{
+				printf("o");
+				a++;	
+			}
+		}
+
+	
+}
+    
